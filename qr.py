34e3603049947bc43b10b20d3e2a1b0f05d727c3
tp1050 <tp1050@gmail.com>
2022-08-10 12:47:28 +0430

    First draft COPPIED OFF gFORg

---
+++ qr.py
@@ -0,0 +1,8 @@
+import qrcode
+data="whatever"
+qr=qrcode.QRCode(version=1,box_size=10,vorder=5)
+qr.add_data(data)
+
+qr.make(fit=True)
+img=qr.make_image(fill_color='red'.back_color='white')
+img.save('myQRcODE.jpg')
